eea857def96e3d0d6fcf519f0d86934416865945
Bryan <draco4king@gmail.com>
2022-04-10 20:15:08 +0900

    feat: prefect server runs

--- run.sh
+++ run.sh
@@ -19,14 +19,25 @@ helm repo add nginx-stable https://helm.nginx.com/stable
 # https://kind.sigs.k8s.io/docs/user/ingress/#ingress-nginx
 # The manifests contains kind specific patches to forward the hostPorts to the ingress controller, set taint tolerations and schedule it to the custom labelled node.
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl annotate ingressclass nginx ingressclass.kubernetes.io/is-default-class=true
 
 #######################################
 ############## DATABASE ###############
 #######################################
-helm upgrade \
+helm upgrade --cleanup-on-fail \
   --install database bitnami/mariadb \
   --namespace data-pipeline \
   --values kubernetes/values.mariadb.yaml
+
+#######################################
+############## PREFECT ################
+#######################################
+helm repo add prefecthq https://prefecthq.github.io/server/
+helm upgrade --cleanup-on-fail \
+ --install pipeline prefecthq/prefect-server \
+ --namespace data-pipeline \
+ --values kubernetes/values.prefect.yaml
+# http://localhost:8000/
 
 #######################################
 ########## JUPYTER NOTEBOOK ###########
@@ -36,4 +47,5 @@ helm upgrade --cleanup-on-fail \
   --install jupyterhub jupyterhub/jupyterhub \
   --namespace data-pipeline \
   --values kubernetes/values.jupyterhub.yaml
+# http://localhost:8000/hub
 # default login -- admin / admin
