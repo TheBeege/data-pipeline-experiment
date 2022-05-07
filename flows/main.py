559ebac8066a33843b068daf4fd4f3e91e2ca800
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2022-05-07 15:19:49 +0900

    feat: Added link info, removed database insert

--- flows/main.py
+++ flows/main.py
@@ -1,4 +1,6 @@
+import json
 import os
+import urllib.parse
 
 import prefect
 from prefect import task, Flow
@@ -14,7 +16,8 @@ def retrieve_tweets():
     response = requests.get(
         'https://api.twitter.com/2/tweets/search/recent',
         params={
-            'query': 'data science',
+            'query': '"data science" has:links',
+            'tweet.fields': 'entities',
             'max_results': '10',
         },
         headers={
@@ -22,6 +25,7 @@ def retrieve_tweets():
         },
     )
     results = response.json()
+    print(json.dumps(results))
     logger.info(f'got {len(results["data"])} tweets')
     return results
 
@@ -30,9 +34,26 @@ def retrieve_tweets():
 def transform_data(response):
     logger = prefect.context.get("logger")
     logger.info('transforming tweet data')
-    data = [[tweet['id'], tweet['text']] for tweet in response['data']]
-    logger.info(f'reformatted {len(data)} records')
-    return data
+    hostname_counts = {}
+    for tweet in response['data']:
+        if 'urls' in tweet['entities']:
+            for url in tweet['entities']['urls']:
+                if 'unwound_url' in url:
+                    hostname = urllib.parse.urlparse(url['unwound_url']).hostname
+                else:
+                    hostname = urllib.parse.urlparse(url['expanded_url']).hostname
+
+                # this hostname has been seen before
+                if hostname in hostname_counts:
+                    # so we increase its count by 1
+                    hostname_counts[hostname] += 1
+                # this hostname has not been seen before
+                else:
+                    # this is the first time seeing it, so set the count to 1
+                    hostname_counts[hostname] = 1
+    logger.info(f'reformatted {len(hostname_counts)} records')
+    logger.info(f'hostname_counts: {json.dumps(hostname_counts)}')
+    return hostname_counts
 
 
 @task(log_stdout=True)
@@ -57,7 +78,7 @@ def store_data(records):
 with Flow("Twitter data") as flow:
     tweets = retrieve_tweets()
     formatted_data = transform_data(tweets)
-    store_data(formatted_data)
+    # store_data(formatted_data)
 
 
 flow.run()
